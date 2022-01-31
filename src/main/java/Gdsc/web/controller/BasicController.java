354802f75b788d307de1cd8821575ac193c16093
yoo hyeong chan <gudcks305@gmail.com>
2022-01-31 11:49:08 +0900

    Update BasicController.java

--- src/main/java/Gdsc/web/controller/BasicController.java
+++ src/main/java/Gdsc/web/controller/BasicController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class BasicController {
     @GetMapping("/")
     public String test() {
-        return "hi hi";
+        return "hi hi ";
     }
 
 }
