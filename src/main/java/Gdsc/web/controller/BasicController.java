fd627cc888e0a78676a4ee702a3dbf0b376948bf
yoo hyeong chan <gudcks305@gmail.com>
2022-02-04 14:33:51 +0900

    test : add testController
    테스트 후 제거

--- src/main/java/Gdsc/web/controller/BasicController.java
+++ src/main/java/Gdsc/web/controller/BasicController.java
@@ -1,14 +1,19 @@
 package Gdsc.web.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class BasicController {
     @GetMapping("/")
     public String test() {
         return "hi hi";
     }
-
+    @GetMapping("/g")
+    public String tes1t() {
+        log.error("test");
+        return "hi";
+    }
 }
