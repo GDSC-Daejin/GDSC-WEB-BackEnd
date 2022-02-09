b780216ac38fe65d4aef4f16430199427d2401d3
rnjstmdals6 <rnjstmdals6@naver.com>
2022-02-10 01:07:58 +0900

    feat AdminApiController 게스트 목록, 멤버 목록 조회 API 추가

--- src/main/java/Gdsc/web/repository/MemberRepository.java
+++ src/main/java/Gdsc/web/repository/MemberRepository.java
@@ -4,10 +4,18 @@ import Gdsc.web.domain.Member;
 import Gdsc.web.domain.OnboardingMember;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     //Optional<Member> findByEmail(String email);
     Optional<Member> findByUsername(String username);
+
+    @Query("select m from Member m where m.role = 'MEMBER' or m.role = 'CORE' or m.role = 'LEAD'")
+    List<Member> findMember();
+
+    @Query("select m from Member m where m.role = 'GUEST'")
+    List<Member> findGUEST();
 }
