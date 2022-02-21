ecc75e69fe82b44c048003c026af789d7921e9d6
yoo hyeong chan <gudcks305@gmail.com>
2022-02-21 18:43:47 +0900

    Update SupportApiController.java

--- src/main/java/Gdsc/web/controller/api/SupportApiController.java
+++ src/main/java/Gdsc/web/controller/api/SupportApiController.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class SupportApiController {
     private final SupportService supportService;
-    @GetMapping("/api/support/limit")
+    @GetMapping("/support/limit")
     public ResponseDto<SupportDto> list() {
         return new ResponseDto<>(HttpStatus.OK, supportService.지원제한(), "지원제한 정보");
     }
-    @PutMapping("/api/support/limit/update")
+    @PutMapping("/api/admin/support/limit/update")
     public ResponseDto<Integer> update(@RequestBody SupportDto supportDto){
         supportService.지원제한업데이트(supportDto);
         return new ResponseDto<>(HttpStatus.OK, 1, "업데이트 성공");
