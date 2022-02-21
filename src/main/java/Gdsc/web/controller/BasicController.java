f5bd7dd929c3ed7a66323770fafbab27e375e18a
yoo hyeong chan <gudcks305@gmail.com>
2022-02-21 19:36:45 +0900

    g

--- src/main/java/Gdsc/web/controller/BasicController.java
+++ src/main/java/Gdsc/web/controller/BasicController.java
@@ -1,11 +1,15 @@
 package Gdsc.web.controller;
 
 
-
 import Gdsc.web.dto.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import lombok.extern.slf4j.Slf4j;
+
+import Gdsc.web.dto.ApiResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Controller;
 
 
@@ -13,7 +17,7 @@ import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class BasicController {
     @GetMapping("/")
