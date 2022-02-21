e0e5356a89fa3217e752de83896fbfa18a6ac712
yoo hyeong chan <gudcks305@gmail.com>
2022-02-21 19:27:12 +0900

    Revert "Merge branch 'master' into develop"
    This reverts commit d878960950bc814349d27303db10acaca5b07680, reversing
    changes made to 1e368ce6ab3b58de5551f0deda909847f195e059.

--- src/main/java/Gdsc/web/controller/BasicController.java
+++ src/main/java/Gdsc/web/controller/BasicController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-@Slf4j
+
 @RestController
 public class BasicController {
 
