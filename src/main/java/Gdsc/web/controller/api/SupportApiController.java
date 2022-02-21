58064e834c824e8609548d9b4fe78335a8a9b960
yoo hyeong chan <gudcks305@gmail.com>
2022-02-21 18:47:34 +0900

    fix : api error

--- src/main/java/Gdsc/web/controller/api/SupportApiController.java
+++ src/main/java/Gdsc/web/controller/api/SupportApiController.java
@@ -11,11 +11,11 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class SupportApiController {
     private final SupportService supportService;
-    @GetMapping("/support/limit")
+    @GetMapping("/api/support/limit")
     public ResponseDto<SupportDto> list() {
         return new ResponseDto<>(HttpStatus.OK, supportService.지원제한(), "지원제한 정보");
     }
-    @PutMapping("/api/admin/support/limit/update")
+    @PutMapping("/api/support/limit/update")
     public ResponseDto<Integer> update(@RequestBody SupportDto supportDto){
         supportService.지원제한업데이트(supportDto);
         return new ResponseDto<>(HttpStatus.OK, 1, "업데이트 성공");
