79b77d1b57abb772fa6fe7c14e119b35887795e1
yoo hyeong chan <gudcks305@gmail.com>
2022-02-24 15:42:54 +0900

    feat : 멤버 정보 업데이트 기능
    더티 체킹 방법으로 할 경우 개발자가 원하지 않는 방식의 데이터 입력을 방지 할 수 있음!

--- src/main/java/Gdsc/web/dto/WarningDto.java
+++ src/main/java/Gdsc/web/dto/WarningDto.java
@@ -1,10 +1,14 @@
 package Gdsc.web.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class WarningDto {
+    @ApiModelProperty(example = "경고재목")
     String title;
+    @ApiModelProperty(example = "경고내용")
     String content;
+    @ApiModelProperty(example = "누구한테 줄건지")
     String ToUser;
 }
--- src/main/java/Gdsc/web/repository/memberinfo/JpaMemberInfoRepository.java
+++ src/main/java/Gdsc/web/repository/memberinfo/JpaMemberInfoRepository.java
@@ -4,5 +4,8 @@ import Gdsc.web.entity.Member;
 import Gdsc.web.entity.MemberInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaMemberInfoRepository extends JpaRepository<MemberInfo, Integer> {
+    Optional<MemberInfo> findByUserID(String userId);
 }
