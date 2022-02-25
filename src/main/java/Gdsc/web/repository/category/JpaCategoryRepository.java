d139e7dbdc3b6914656ccca1d38b7ae65e5bef6f
yoo hyeong chan <gudcks305@gmail.com>
2022-02-25 15:52:26 +0900

    feat : category 추가 삭제 기능 API  추가 , JPA auditing 수정
    업데이트는 categoryDTO 를 이용함
    수정 일자가 입력 안되는 문제 해결

--- src/main/java/Gdsc/web/repository/category/JpaCategoryRepository.java
+++ src/main/java/Gdsc/web/repository/category/JpaCategoryRepository.java
@@ -4,5 +4,9 @@ import Gdsc.web.entity.Category;
 import Gdsc.web.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface JpaCategoryRepository  extends JpaRepository<Category,Integer> {
+
+    Optional<Category> findByCategoryName(String categoryName);
 }
