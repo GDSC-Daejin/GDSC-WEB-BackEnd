83058ec774b9d345aa50a1c951a65a92dee057e7
yoo hyeong chan <gudcks305@gmail.com>
2022-03-12 12:16:27 +0900

    test : 기본 테스트 코드 입니다.
    기본 테스트 코드 입니다. JPA 없는 곳에선 문제가 없네요 아직

--- src/test/java/Gdsc/web/controller/BasicControllerTest.java
+++ src/test/java/Gdsc/web/controller/BasicControllerTest.java
@@ -1,14 +1,25 @@
 package Gdsc.web.controller;
 
 import Gdsc.web.config.SecurityConfig;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(controllers = BasicController.class,
@@ -17,10 +28,23 @@ import org.springframework.test.web.servlet.MockMvc;
         })
 class BasicControllerTest {
     @Autowired
+    private WebApplicationContext context;
+    @Autowired
     private MockMvc mvc;
 
-
+    @Before
+    void setup(){
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+    @WithMockUser(roles = "MEMBER")
     @Test
-    void test1() {
+    void test1() throws Exception {
+        String hello = "hi hi!!";
+
+        mvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
 }
