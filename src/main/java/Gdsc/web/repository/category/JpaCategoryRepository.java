2ee630a9a9afef6740e3623cbef678ba48f52aad
yoo hyeong chan <gudcks305@gmail.com>
2022-03-20 16:57:38 +0900

    fix : post API에서 카테고리 업데이트 및 추가 오류 해결

--- src/main/java/Gdsc/web/repository/category/JpaCategoryRepository.java
+++ src/main/java/Gdsc/web/repository/category/JpaCategoryRepository.java
@@ -2,6 +2,7 @@ package Gdsc.web.repository.category;
 
 import Gdsc.web.entity.Category;
 import Gdsc.web.entity.Post;
+import io.swagger.models.auth.In;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -9,4 +10,6 @@ import java.util.Optional;
 public interface JpaCategoryRepository  extends JpaRepository<Category,Integer> {
 
     Optional<Category> findByCategoryName(String categoryName);
+
+    Category findByCategoryId(int categoryId);
 }
