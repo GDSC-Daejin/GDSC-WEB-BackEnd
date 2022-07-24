bc17603102930f3d7157525fbd320c39100e46ae
yoo hyeong chan <gudcks305@gmail.com>
2022-07-24 18:09:55 +0900

    Update TokenValidateInterceptor.java

--- src/main/java/Gdsc/web/oauth/handler/TokenValidateInterceptor.java
+++ src/main/java/Gdsc/web/oauth/handler/TokenValidateInterceptor.java
@@ -25,7 +25,9 @@ public class TokenValidateInterceptor implements HandlerInterceptor {
 
             String accessToken = HeaderUtil.getAccessToken(request);
             AuthToken authToken = tokenProvider.convertAuthToken(accessToken);
+            log.info(accessToken);
             if(!authToken.validate()){
+                log.info("Unauthorized" + accessToken + " "+ request.getRemoteUser());
                 response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
                 return false;
             }
