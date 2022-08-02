a2afd921f242499e7f7dd3f40b6bb7f9c69b2fd5
yoo hyeong chan <gudcks305@gmail.com>
2022-08-02 15:32:14 +0900

    Update MemberService.java

--- src/main/java/Gdsc/web/member/service/MemberService.java
+++ src/main/java/Gdsc/web/member/service/MemberService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
@@ -29,7 +30,7 @@ public class MemberService {
                 AuthServerURL +"/internal/member/api/memberInfo",
                 MemberInfoResponseServerDto[].class);
         assert memberInfos != null;
-        return List.of(memberInfos);
+        return Arrays.asList(memberInfos);
     }
 
 
