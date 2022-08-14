7cf475f8953ec37d122c647f35cefcab65668e71
rnjstmdals6 <rnjstmdals6@naver.com>
2022-08-15 06:05:43 +0900

    [REFACT] 카테고리 리팩토링
    1. 반환타입 수정
    Response<Object>로 지정할 시 하이라이트 제거되며 기존 반환타입과 차이가 없음.
    2. 카테고리 삭제 시 RequestBody에서 Entity를 받는 것보다 Request로 받는게 좋아보임

--- src/main/java/Gdsc/web/category/controller/CategoryApiController.java
+++ src/main/java/Gdsc/web/category/controller/CategoryApiController.java
@@ -29,33 +29,36 @@ public class CategoryApiController {
             @ApiResponse(responseCode = "400", description = "카테고리 정보 조회 실패")
     })
     @GetMapping("api/v1/category")
-    public Response<List<Category>> categoryList(){
+    public Response<Object> categoryList(){
         return Response.success("data", categoryService.카테고리목록());
     }
+
     @Operation(summary = "카테고리 정보 추가", description = "카테고리 정보 추가")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = CategoryRequestDto.class))),
             @ApiResponse(responseCode = "400", description = "카테고리 정보 추가 실패")
     })
     @PostMapping("api/admin/v1/category/add")
-    public Response addCategory(@RequestBody CategoryRequestDto categoryRequestDto){
+    public Response<Object> addCategory(@RequestBody CategoryRequestDto categoryRequestDto){
 
         categoryService.카테고리추가(categoryRequestDto);
 
         return Response.success("message", "Success");
     }
+
     @Operation(summary = "카테고리 정보 삭제", description = "카테고리 정보 삭제")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "카테고리 정보 삭제 성공"),
             @ApiResponse(responseCode = "400", description = "카테고리 정보 삭제 실패")
     })
     @DeleteMapping("api/admin/v1/category/delete")
-    public Response deleteCategory(@RequestBody Category category) {
+    public Response<Object> deleteCategory(@RequestBody CategoryRequestDto categoryRequestDto) {
 
-        categoryService.카테고리삭제(category);
+        categoryService.카테고리삭제(categoryRequestDto);
 
        return Response.success("message", "Success");
     }
+
     @Operation(summary = "카테고리 정보 수정", description = "카테고리 정보 수정")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", content = @Content(schema = @Schema(implementation = CategoryUpdateDto.class))),
@@ -64,14 +67,10 @@ public class CategoryApiController {
     @PutMapping("api/admin/v1/category/update")
     // DTO 를 사용한 이유는 category객체로 받을 경우 데이터 보내는 것이 너무 더러워짐
     //  ex : { Category:{ "GG": "GG", "@$":"GG", "geg" :"GGG" } , String : #%## }
-    public Response updateCategory(@RequestBody CategoryUpdateDto category) {
+    public Response<Object> updateCategory(@RequestBody CategoryUpdateDto category) {
 
         categoryService.카테고리업데이트(category);
 
         return Response.success("message", "Success");
     }
-
-
-
-
 }
