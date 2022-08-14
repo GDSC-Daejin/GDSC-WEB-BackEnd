eedc0a7a9dfa5651f992b05671a1c536c77218fd
rnjstmdals6 <rnjstmdals6@naver.com>
2022-08-15 06:13:45 +0900

    [REFACT] 카테고리 서비스 리팩토링
    1. 카테고리 Entity 리스트 조회 후 stream을 사용하여 Dto 리스트에 담음
    
    2. 유효성 검사 추가

--- src/main/java/Gdsc/web/category/service/CategoryService.java
+++ src/main/java/Gdsc/web/category/service/CategoryService.java
@@ -1,48 +1,61 @@
 package Gdsc.web.category.service;
 
 import Gdsc.web.category.dto.CategoryRequestDto;
+import Gdsc.web.category.dto.CategoryResponseDto;
 import Gdsc.web.category.dto.CategoryUpdateDto;
 import Gdsc.web.category.entity.Category;
 import Gdsc.web.category.repository.JpaCategoryRepository;
 import lombok.RequiredArgsConstructor;
+import okhttp3.internal.http2.ErrorCode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class CategoryService {
     private final JpaCategoryRepository jpaCategoryRepository;
+
     @Transactional(readOnly = true)
-    public List<Category> 카테고리목록(){
-        return jpaCategoryRepository.findAll();
+    public List<CategoryResponseDto> 카테고리목록(){
+        // 저장된 카테고리 Entity 리스트를 모두 조회하여 Dto 리스트로 변환한 후 반환합니다.
+
+        return jpaCategoryRepository.findAll().stream()
+                .map(CategoryResponseDto::new)
+                .collect(Collectors.toList());
     }
 
     @Transactional
-    public void 카테고리추가(CategoryRequestDto CategoryRequestDto) {
-        validateDuplicateCategoryName(CategoryRequestDto.getCategoryName());
-        Category category = new Category();
-        category.setCategoryName(CategoryRequestDto.getCategoryName());
+    public void 카테고리추가(CategoryRequestDto categoryRequestDto) {
+        // 카테고리 이름이 중복되는지 확인합니다.
+        final Category result = jpaCategoryRepository.findByCategoryName(categoryRequestDto.getCategoryName()).orElse(null);
+
+        if (result != null) {
+            throw new IllegalArgumentException("이미 존재하는 카테고리 입니다.");
+        }
+
+        Category category = new Category(categoryRequestDto);
         jpaCategoryRepository.save(category);
     }
+
     @Transactional
-    public void 카테고리삭제(Category category){
+    public void 카테고리삭제(CategoryRequestDto categoryRequestDto){
+        // 삭제할 카테고리가 존재하는 지 확인합니다.
+        Category category = jpaCategoryRepository.findByCategoryName(categoryRequestDto.getCategoryName()).orElseThrow(
+                ()-> new IllegalArgumentException("찾을 수 없는 카테고리 입니다."));
+
         jpaCategoryRepository.deleteByCategoryName(category.getCategoryName());
     }
+
     @Transactional
     public void 카테고리업데이트(CategoryUpdateDto categoryUpdateDto){
+        // 수정할 카테고리가 존재하는 지 확인합니다.
         Category category = jpaCategoryRepository.findByCategoryName(categoryUpdateDto.getCategoryName()).orElseThrow(
                 ()-> new IllegalArgumentException("찾을 수 없는 카테고리 입니다."));
 
-        validateDuplicateCategoryName(categoryUpdateDto.getUpdateCategoryName());
-        category.setCategoryName(categoryUpdateDto.getUpdateCategoryName());
-    }
-    private void validateDuplicateCategoryName(String categoryName) {
-        jpaCategoryRepository.findByCategoryName(categoryName)
-                .ifPresent(m-> {
-                    throw new IllegalStateException("이미 존재하는 카테고리 입니다.");
-                });
+        category.update(categoryUpdateDto);
     }
 }
