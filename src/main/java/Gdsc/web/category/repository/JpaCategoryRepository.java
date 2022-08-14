bcf6cd2c23aa28d5c8a2bcbf0348312f870653de
rnjstmdals6 <rnjstmdals6@naver.com>
2022-08-15 06:12:00 +0900

    [CHORE] 카테고리 JPA 매서드 삭제
    사용하지않는 JPA 매서드 삭제

--- src/main/java/Gdsc/web/category/repository/JpaCategoryRepository.java
+++ src/main/java/Gdsc/web/category/repository/JpaCategoryRepository.java
@@ -8,6 +8,5 @@ import java.util.Optional;
 public interface JpaCategoryRepository  extends JpaRepository<Category,Integer> {
 
     Optional<Category> findByCategoryName(String categoryName);
-    Category findByCategoryId(int categoryId);
     void deleteByCategoryName(String categoryName);
 }
